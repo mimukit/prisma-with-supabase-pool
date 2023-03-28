23ee1f88cfd9a047230fbae78375acd6ccf7a54d
Mukitul Islam Mukit <hellomukit@gmail.com>
2023-03-28 21:27:07 +0600

    feat: add getDrizzle api

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,17 +1,27 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { z } from "zod";
-
 import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
-);
 import {
   createTRPCRouter,
   publicProcedure,
   protectedProcedure,
 } from "~/server/api/trpc";
+
+import { pgTable, text, date } from "drizzle-orm/pg-core";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
+
+export const exampleTable = pgTable("Example", {
+  id: text("id").primaryKey(),
+  name: text("name"),
+  createdAt: date("createdAt").defaultNow(),
+  updatedAt: date("updatedAt"),
+});
+
+const supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!
+);
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
@@ -28,6 +38,18 @@ export const exampleRouter = createTRPCRouter({
 
   getSupa: publicProcedure.query(() => {
     return supabase.from("Example").select().limit(100);
+  }),
+
+  getDrizzle: publicProcedure.query(async () => {
+    const drizzleClient = new Client({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    await drizzleClient.connect();
+
+    const db = drizzle(drizzleClient);
+
+    return db.select().from(exampleTable).limit(100);
   }),
 
   getSecretMessage: protectedProcedure.query(() => {
